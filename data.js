f4a8e573ed0d6e764d73dbbfb0de8ca489981782
nicole11robayo <113997899+nicole11robayo@users.noreply.github.com>
2022-10-18 13:17:59 -0500

    Updates

---
+++ data.js
@@ -0,0 +1,29 @@
+// estas funciones son de ejemplo
+// const valorLibros=document.getElementById("data").value= data.books;
+// valorLibros="";
+export const example = () => {
+
+   
+  
+  /*
+  fetch("./data/harrypotter/harry.json")
+  .then(Response => Response.json())
+  .then(data => console.log(data))
+
+  
+  const mostrarData= (data.books)=>{
+    console.log(data);
+    let body="";
+    for(let i=0,)
+  }
+  */
+
+  return 'example';
+};
+
+
+
+export const anotherExample = () => {
+  return 'OMG';
+};
+
