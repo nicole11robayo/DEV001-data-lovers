820110e18e118947ec7761dd1ac7735daa9040af
nicole11robayo <113997899+nicole11robayo@users.noreply.github.com>
2022-11-01 16:58:05 -0500

    Updates

--- data.js
+++ data.js
@@ -5,7 +5,36 @@
   export const anotherExample = () => {
     return 'OMG';
   };*/
-  import data from './data/harrypotter/harry.js';
+  //import data from './data/harrypotter/harry.js';
+
+  export const encontrarId = (id, dataHarry) => {
+    const encontrarDataId = dataHarry.find(
+    (tarjeta)=> tarjeta.id == id);
+    //console.log(findbyidPokemon);
+    //console.log(encontrarDataId);
+    return encontrarDataId;
+  };
+
+  export const ordenarAaZ= (dataHarry)=>{
+    const copiaData=[...dataHarry];
+    const sortAaZ= copiaData.sort((a, b) =>{
+      if (a.title< b.title){
+        return -1;
+      }
+    });
+    return sortAaZ; 
+  };
+
+  export const ordenarFecha= (dataHarry)=>{
+    const copiaData=[...dataHarry];
+    const sortFecha= copiaData.sort((a,b)=>{
+      if(a.relaseDay < b.relaseDay){
+        return -1;
+      }
+    });
+    return sortFecha;
+  };
+  
 
   
   
--- main.js
+++ main.js
@@ -1,359 +1,132 @@
 import data from './data/harrypotter/harry.js';
 
+import { llamarData , llamarModal} from './mostrardata.js';
+import { encontrarId, ordenarAaZ, ordenarFecha } from './data.js';
+
+
+
+
 // eslint-disable-next-line no-unused-vars
 
 
-const dataLibros=data.books;
-const dataPersonajes=data.characters;
-const dataPociones=data.potions;
-const dataHechizos=data.spells;
-
-/*const datas=[];
-for(let i=0; i<8; i++){
-    datas.push(dataLibros[i].title)
-}
-const tt=datas.sort();
-function verMas(){
-    console.log("verMas")
-}*/
+//const dataLibros=data.books;
+//const dataPersonajes=data.characters;
+//const dataPociones=data.potions;
+//const dataHechizos=data.spells;
 
 
-const caja= document.getElementById("libros");
-const contenedorCeldas=document.createElement("div");
-contenedorCeldas.className="contenedorCeldas";
-
-
-dataLibros.forEach(l =>{
-    // eslint-disable-next-line no-unused-vars
-    
-    const celdas=document.createElement("div");
-    celdas.className="celdas";
+let copiaDataLibros= [...data.books];
+llamarData(copiaDataLibros);
+mostrarModal();
 
 
 
-    let img = document.createElement('img');
-    img.src = l.portada;
-    img.style.height= "400px";
-    img.style.display="block";
-    img.style.margin="auto";
-    img.style.borderRadius="10px";
-    celdas.appendChild(img);
-
-    const h1Celdas=document.createElement("h1");
-    h1Celdas.className="h1Celdas";
-    h1Celdas.innerHTML=l.title;
-    celdas.appendChild(h1Celdas);
-
-    
-    const button=document.createElement("input");
-    button.type="button";
-    button.value="Ver más";
-    button.className="buttonVerMas";
-    button.label="Ver más";
-    celdas.appendChild(button);
-
-    
-    
-
-    for (let i=0;i<8; i++){
-      let llamarbutton=celdas.lastElementChild;
-      llamarbutton.id=[i];
-      console.log(llamarbutton);
-      
+function mostrarModal(){
+  const celdas=document.querySelectorAll(".celdas");
+  const span = document.getElementsByClassName("close")[0];
+  celdas.forEach(celda  =>{
+    celda.addEventListener("click", () =>{
+      document.querySelector("#myModal").style.display="flex";
+      const idtarjeta= celda.className.split(" ")[0];
+      //console.log(idtarjeta)
+      llamarModal(encontrarId(idtarjeta, data.books));
     }
-
-    const modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-
-// Get the <span> element that closes the modal
-    const span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-    button.onclick = function() {
-      modal.style.display = "block";
-    }
-
-    // When the user clicks on <span> (x), close the modal
+    );
+    const contenedor1=document.getElementById("libros1");
     span.onclick = function() {
-      modal.style.display = "none";
+      document.querySelector("#myModal").style.display = "none";
+      contenedor1.innerHTML=" ";
     }
-
-    // When the user clicks anywhere outside of the modal, close it
     window.onclick = function(event) {
-      if (event.target == modal) {
-        modal.style.display = "none";
+      if (event.target == document.querySelector("#myModal")) {
+        document.querySelector("#myModal").style.display = "none";
+        contenedor1.innerHTML=" ";
       }
     }
+  })
+}
 
+let contenedor1= document.getElementById("libros");
+
+const selector=document.getElementById("select");
+selector.addEventListener("change", () => {
+  contenedor1.innerHTML=" ";
+  switch (selector.value) {
+    case "all":
+
+      llamarData(data.books);
+      
+      break;
+    case "ordenarAZ":
+      llamarData(ordenarAaZ(data.books));
+      break;
+    case "ordenarZA":
+      llamarData(ordenarAaZ(data.books).reverse());
+      break;
+    case "recienteAntiguo":
+      llamarData(ordenarFecha(data.books));
+      break;
+    case "antiguoReciente":
+      llamarData(ordenarFecha(data.books).reverse());
+      break;
     
-    contenedorCeldas.appendChild(celdas);  
-}) 
 
-caja.appendChild(contenedorCeldas);
-
-
-
-/*const modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-const btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-const span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
   }
-}*/
+  mostrarModal();
+});
 
 
 
-const caja1= document.getElementById("libros1");
-const contenedorCeldas1=document.createElement("div");
 
-contenedorCeldas1.className="contenedorCeldas1";
 
-dataLibros.forEach(l =>{
-  
     
-    const celdas1=document.createElement("div");
-    celdas1.className="celdas1";
 
-    let img1 = document.createElement('img');
-    img1.className="img1";
-    img1.src = l.portada;
-    celdas1.appendChild(img1);
-
-    const h1Celdas1=document.createElement("h1");
-    h1Celdas1.className="h1Celdas1";
-    h1Celdas1.innerHTML=l.title;
-
-    celdas1.appendChild(h1Celdas1);
-
-    const p1Celdas1=document.createElement("p");
-    p1Celdas1.className="p1Celdas1";
-    p1Celdas1.innerHTML= "<b>Autora: </b>"+ l.author;
-
-    celdas1.appendChild(p1Celdas1);
-
-    const p2Celdas1=document.createElement("p");
-    p2Celdas1.className="p2Celdas1";
-    p2Celdas1.innerHTML="<b>Fecha: </b>" +l.releaseDay;
-
-
-    celdas1.appendChild(p2Celdas1);
-
-    const p3Celdas1=document.createElement("p");
-    p3Celdas1.className="p3Celdas1";
-    p3Celdas1.innerHTML="<b>Descripción:</b> "+ l.description;
-
-    celdas1.appendChild(p3Celdas1);
-
-
-
-    contenedorCeldas1.appendChild(celdas1);
-})
-caja1.appendChild(contenedorCeldas1);
-
-
-for (let i=0;i<8; i++){
-  let llamardiv=contenedorCeldas1.children[i];
-  llamardiv.id=(i);
-  
-}
-
+/*
 
 const caja4= document.getElementById("Personajes");
-const contenedorCeldas4=document.createElement("div");
 
-contenedorCeldas4.className="contenedorCeldas4";
 
 dataPersonajes.forEach(l =>{
-    // eslint-disable-next-line no-unused-vars
-    
-    
-    const celdas4=document.createElement("div");
-    celdas4.className="celdas4";
-
-    const nameCeldas4=document.createElement("h1");
-    nameCeldas4.className="nameCeldas4";
-    nameCeldas4.innerHTML=l.name;
-    celdas4.appendChild(nameCeldas4);
-
-    
-    const houseCeldas4=document.createElement("p");
-    houseCeldas4.className="houseCeldas4";
-    houseCeldas4.innerHTML= "<b>Casa: </b>" + l.house;
-    celdas4.appendChild(houseCeldas4);
-
+  let dataPersonas= `<div class= "contenedorCeldas4">
+  <div class= "celdas4">
+  <h1 class= "nameCeldas4">${l.name}</h1>
+  <p class="descrpcionCeldas4"><b>Casa: </b> ${l.house}</p>
+  </div>
   
-    
-   /* const buttonData=document.getElementById("buttonData")
-    const button=document.createElement("input");
-    button.type="button";
-    button.value="Ver más";
-    button.className="buttonVerMas";
-    button.label="Ver más";
-    buttonData.appendChild(button);
-    celdas.appendChild(buttonData);*/
+  </div>`
+  
 
-    contenedorCeldas4.appendChild(celdas4);  
-}) 
-caja4.appendChild(contenedorCeldas4);
+  caja4.insertAdjacentHTML("beforeend", dataPersonas);
+});
 
 
+dataPociones.forEach(l=>{
+const caja2=document.getElementById("pociones");
 
+const plantillaPociones=   
+`<div class="contenedorPociones">
+<div class="pociones01">
+ <h1 class="idPociones">${l.id}</h1><br>
+  <h1 class="namePociones"> ${ l.name }</h1>
+  <p class="desPociones><b>Descripción: </b>${ l.description}<br> 
+ </div>
+ </div>`;
+ 
+ caja2.insertAdjacentHTML("beforeend", plantillaPociones);})
 
 
 
+dataHechizos.forEach(l=>{
+const plantillaHechizos=   
+`<div class="contenedorHechizos">
+<div class="hechizos">
+ <h1 class="h1hechizos"><b>Nombre: </b>${l.name}</h1><br>
+  <p class="phechizos"><b>Pronunciación: </b> ${ l.pronunciation }</p>
+  <p><b>Descripción: </b>${ l.description}<br> 
+ </div>`;
+document.body.innerHTML = plantillaHechizos;})
 
-const caja2= document.getElementById("pociones");
-const contenedorCeldas2=document.createElement("div");
+*/
 
-contenedorCeldas2.className="contenedorCeldas2";
-
-dataPociones.forEach(l =>{
-    // eslint-disable-next-line no-unused-vars
-    
-    
-    const celdas2=document.createElement("div");
-    celdas2.className="celdas2";
-
-    const idCeldas2=document.createElement("h1");
-    idCeldas2.className="idCeldas2";
-    idCeldas2.innerHTML=l.id;
-    celdas2.appendChild(idCeldas2);
-
-    
-    const nameCeldas2=document.createElement("h1");
-    nameCeldas2.className="nameCeldas2";
-    nameCeldas2.innerHTML= l.name;
-    celdas2.appendChild(nameCeldas2);
-
-    
-    const desCeldas2=document.createElement("p");
-    desCeldas2.className="desCeldas2";
-    desCeldas2.innerHTML= "<b>Descripción: </b>" + l.description;
-    celdas2.appendChild(desCeldas2);
-    
-   /* const buttonData=document.getElementById("buttonData")
-    const button=document.createElement("input");
-    button.type="button";
-    button.value="Ver más";
-    button.className="buttonVerMas";
-    button.label="Ver más";
-    buttonData.appendChild(button);
-    celdas.appendChild(buttonData);*/
-
-    contenedorCeldas2.appendChild(celdas2);  
-}) 
-caja2.appendChild(contenedorCeldas2);
-
-
-
-
-const caja3= document.getElementById("hechizos");
-const contenedorCeldas3=document.createElement("div");
-
-contenedorCeldas3.className="contenedorCeldas3";
-
-dataHechizos.forEach(l =>{  
-    const celdas3=document.createElement("div");
-    celdas3.className="celdas3";
-
-    const h1Celdas3=document.createElement("h1");
-    h1Celdas3.className="h1Celdas3";
-    h1Celdas3.innerHTML="<b>Nombre: </b>"+ l.name;
-    celdas3.appendChild(h1Celdas3);
-
-    const pCeldas3=document.createElement("p");
-    pCeldas3.className="pCeldas3";
-    pCeldas3.innerHTML= "<b>Pronunciación: </b>"+ l.pronunciation;
-    celdas3.appendChild( pCeldas3);
-    
-   
-
-    contenedorCeldas3.appendChild(celdas3);  
-}) 
-
-caja3.appendChild(contenedorCeldas3);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// When the user clicks on <span> (x), close the modal
-
-
-// When the user clicks anywhere outside of the modal, close it
 
 
